5a7d24138d6836e12221b15e8a05882ec6c657ac
kuchobang <obangoriemi@gmail>
2024-05-09 12:22:36 +0300

    creating navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+
+function Navbar(){
+
+
+    return(
+        <div>
+            <nav>
+                <h1>Blog</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Create</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
